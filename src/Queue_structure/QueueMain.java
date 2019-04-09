94427cfa3420c31a42f034a7bfebda7793029153
handnew04 <yamyam1575@gmail.com>
2019-04-09 19:08:01 +0900

    주석 달기

--- src/Queue_structure/QueueMain.java
+++ src/Queue_structure/QueueMain.java
@@ -12,38 +12,41 @@ class Queue<T> {
 
     private Node<T> first = null;
     private Node<T> last = null;
-    private Node<T> pointer = null;
 
-    public void add(T d) {
+    public void add(T d) { //추가
         Node<T> item = new Node<>(d);
-        if (first == null) {
-            first = item;
-            last = first;
+        if (first == null) { // 큐에 아무것도 없을 때
+            first = item; //first 에 아이템이 들어가고
+            last = first; //하나 뿐이기에 last 와 first 가 같은 노드
         } else {
-            last.next = item;
-            last = item;
+            last.next = item; // 이미 노드가 있다면 기존 last 노드의 next에 아이템을 넣고
+            last = item; // 아이템이 새 last 가 된다
         }
     }
 
-    public T remove() {
-        T tmp = first.data;
-        if (first != last) {
-            first = first.next;
-            return tmp;
+    public T remove() { //삭제 -> first 삭제
+        if (first == null) { // 큐에 아무것도 없을때
+            return null;
         }
-        first = null;
+        T tmp = first.data; //출력할 ,삭제 될 노드
+
+        if (first != last) { // 노드가 두개 이상일 때
+            first = first.next; //first 에 그 다음의 노드를 넣어줌
+            return tmp; //삭제된 노드의 데이터 출력
+        }
+        first = null; //노드가 하나일 때 : 둘다 null로 만들고 출력
         last = null;
         return tmp;
     }
 
     public T peek() {
         if (first != null) {
-            return first.data;
+            return first.data; //first의 데이터 출력
         }
         return null;
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty() { //비었는지 체크
         if (first == null) {
             return true;
         }
@@ -60,14 +63,14 @@ public class QueueMain {
         item.add(3);
         item.add(4);
         item.add(5);
-        System.out.println("first : "+item.peek());
-        System.out.println("remove : "+item.remove());
-        System.out.println("remove : "+item.remove());
-        System.out.println("first : "+item.peek());
+        System.out.println("first : " + item.peek());
+        System.out.println("remove : " + item.remove());
+        System.out.println("remove : " + item.remove());
+        System.out.println("first : " + item.peek());
         System.out.println(item.isEmpty());
-        System.out.println("remove : "+item.remove());
-        System.out.println("remove : "+item.remove());
-        System.out.println("remove : "+item.remove());
+        System.out.println("remove : " + item.remove());
+        System.out.println("remove : " + item.remove());
+        System.out.println("remove : " + item.remove());
         System.out.println(item.isEmpty());
     }
 }
